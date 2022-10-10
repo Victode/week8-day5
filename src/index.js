0b3dbc9648485bd8ff3278a119d871a87d50e16f
Victor Llagas <rava44311@gmail.com>
2022-10-10 13:30:07 -0500

    fixed authentication

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import { AuthProvider } from './contexts/AuthProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <AuthProvider> */}
+    <AuthProvider>
       <DataProvider>
         <App />
       </DataProvider>
-    {/* </AuthProvider> */}
+    </AuthProvider>
   </React.StrictMode>
 );
 
